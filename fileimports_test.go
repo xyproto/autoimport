df132cd8985e56fb4e33ffc109030f86e3d7a318
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-14 15:42:09 +0200

    Add tests

---
+++ fileimports_test.go
@@ -0,0 +1,67 @@
+package autoimport
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+// Sample implementation of ImportMatcher for the purpose of testing.
+type SampleImportMatcher struct{}
+
+func (s *SampleImportMatcher) StarPath(word string) (string, string) {
+	switch word {
+	case "ArrayList":
+		return "ArrayList", "java.util.*"
+	case "HashMap":
+		return "HashMap", "java.util.*"
+	default:
+		return "", ""
+	}
+}
+
+func TestFileImports(t *testing.T) {
+	// Mock java content
+	javaContent := `
+package com.example;
+
+import java.util.Map;
+import java.util.List;
+
+public class Sample {
+    List<String> list = new ArrayList<>();
+    Map<String, String> map = new HashMap<>();
+}
+`
+	// Write to a temporary file
+	tmpfile, err := ioutil.TempFile("", "sample.*.java")
+	if err != nil {
+		t.Fatalf("Could not create temporary file: %v", err)
+	}
+	defer os.Remove(tmpfile.Name()) // cleanup
+
+	_, err = tmpfile.Write([]byte(javaContent))
+	if err != nil {
+		t.Fatalf("Could not write to temporary file: %v", err)
+	}
+	tmpfile.Close()
+
+	// Create instance of our test ImportMatcher
+	impl, err := New(true)
+	if err != nil {
+		t.Fatalf("Error initializing the import matcher: %v", err)
+	}
+
+	// Get imports
+	imports, err := impl.FileImports(tmpfile.Name())
+	if err != nil {
+		t.Fatalf("Error generating imports: %v", err)
+	}
+
+	// Expected imports based on the provided implementation of ImportMatcher
+	expected := `import java.util.*; // ArrayList, HashMap, List, Map`
+
+	if imports != expected {
+		t.Errorf("Expected:\n%s\nGot:\n%s", expected, imports)
+	}
+}
